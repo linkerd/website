db14d21755ed7f917704e44a25712d163b7c1d2c
Travis Beckham <travis@buoyant.io>
2024-09-12 11:00:29 -0500

    Added doc search
    Signed-off-by: Travis Beckham <travis@buoyant.io>

---
+++ linkerd.io/assets/js/search.js
@@ -0,0 +1,111 @@
+window.addEventListener(
+  "DOMContentLoaded",
+  function (event) {
+    let index = null;
+    let lookup = null;
+    let query = null;
+    const target = document.querySelector(".search-results");
+    const init = () => {
+      const url = new URL(window.location);
+      const q = url.searchParams.get("q");
+      if (q) {
+        query = q;
+        loadSearchIndex();
+      } else {
+        window.location = "/";
+      }
+    };
+    const loadSearchIndex = () => {
+      const request = new XMLHttpRequest();
+      request.open("GET", "/search.json");
+      request.responseType = "json";
+      request.addEventListener(
+        "load",
+        function (event) {
+          lookup = {};
+          index = lunr(function () {
+            this.ref("uri");
+            this.field("title");
+            this.field("content");
+            this.field("description");
+            this.field("keywords", {boost: 5});
+            for (let doc of request.response) {
+              this.add(doc);
+              lookup[doc.uri] = doc;
+            }
+          });
+          renderSearchResults();
+        },
+        false,
+      );
+      request.addEventListener("error", renderError, false);
+      request.send(null);
+    };
+    const renderSearchResults = () => {
+      const results = index.search(query);
+      // Remove loader
+      target.innerHTML = "";
+      // Render header
+      let template = document.getElementById("search-result-header-template");
+      let node = template.content.cloneNode(true);
+      let txt = "";
+      if (results.length == 0) {
+        txt = "No results found for";
+      } else if (results.length == 1) {
+        txt = "Found one result for";
+      } else {
+        txt = `Found ${results.length} results for`;
+      }
+      node.querySelector(".search-result-header__text").textContent = txt;
+      node.querySelector(".search-result-header__query").textContent = query;
+      if (results.length > 0) {
+        const empty = node.querySelector(".search-result-header__empty");
+        empty.parentNode.removeChild(empty);
+      }
+      target.appendChild(node);
+      // Render results
+      template = document.getElementById("search-result-entry-template");
+      for (let result of results) {
+        let doc = lookup[result.ref];
+        //console.log(`${doc.uri}: ${result.score}`);
+        let node = template.content.cloneNode(true);
+        if (doc.isLinkerd) {
+          node.querySelector(".search-result-entry").classList.add("search-result-entry--linkerd");
+          node.querySelector(".search-result-entry__link").setAttribute("target", "_blank");
+        }
+        if (Array.isArray(doc.ancestors)) {
+          node.querySelector(".search-result-entry__section").textContent = doc.ancestors.join(" / ");
+        }
+        node.querySelector(".search-result-entry__link").href = doc.uri;
+        node.querySelector(".search-result-entry__link").textContent = doc.title;
+        node.querySelector(".search-result-entry__summary").textContent = truncateWords(doc.content, 30);
+        target.appendChild(node);
+      }
+    };
+    const renderError = () => {
+      let template = document.getElementById("search-result-error-template");
+      let node = template.content.cloneNode(true);
+      target.appendChild(node);
+    };
+    const truncateWords = (text, words) => {
+      let match;
+      let result = "";
+      let wordCount = 0;
+      let regexp = /(\S+)(\s*)/g;
+      while ((match = regexp.exec(text))) {
+        wordCount++;
+        if (wordCount <= words) {
+          result += match[0];
+        } else {
+          if (text.length > result.length) {
+            result += "...";
+          }
+          break;
+        }
+      }
+      return result;
+    };
+    init();
+  },
+  false,
+);
